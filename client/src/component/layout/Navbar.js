6b3b48a2d24ef91d0ac750bcf176f93e6173c8c0
ajaojnr5 <abdussamadajao@gmail.com>
2021-03-22 10:24:05 +0100

    react app

---
+++ client/src/component/layout/Navbar.js
@@ -0,0 +1,68 @@
+import React, { Fragment, useContext } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import AuthContext from '../../context/auth/authContext';
+import ContactContext from '../../context/contact/contactContext';
+
+const Navbar = ({ title, icon }) => {
+  const authContext = useContext(AuthContext);
+  const contactContext = useContext(ContactContext);
+
+  const { isAuthenticated, logout, user } = authContext;
+  const { clearContacts } = contactContext;
+  const onLogout = () => {
+    logout();
+    clearContacts();
+  };
+
+  const authLinks = (
+    <Fragment>
+      <li>Welcome {user && user.name} </li>
+      <li>
+        <a href='#!' onClick={onLogout}>
+          <i className='fas fa-sign-out-alt'></i>
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
+    </Fragment>
+  );
+
+  const guestLinks = (
+    <Fragment>
+      <li>
+        <Link to='/login'>
+          <i className='fa fa-sign-in-alt'></i>Login
+        </Link>
+      </li>
+      <li>
+        <Link to='/register'>
+          <i className='fa fa-user'></i> Register
+        </Link>
+      </li>
+      <li>
+        <Link to='/about'>
+          <i className='fa fa-phone-alt'></i> About
+        </Link>
+      </li>
+    </Fragment>
+  );
+  return (
+    <div className='navbar bg-primary'>
+      <h1>
+        <i className={icon} /> {title}
+      </h1>
+      <ul>{isAuthenticated ? authLinks : guestLinks}</ul>
+    </div>
+  );
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
+
+Navbar.defaultProps = {
+  title: 'Contact Keeper',
+  icon: 'fas fa-id-card-alt',
+};
+export default Navbar;
